5d381870854cd86d008614c81647df653a670b7c
Nikita <nikita.gamygin@gmail.com>
2025-12-28 15:22:09 +0100

    color cache

--- src/image_to_minecraft/minecraftifier.py
+++ src/image_to_minecraft/minecraftifier.py
@@ -68,14 +68,21 @@ def converter_bytes(
 
     new_im = Image.new('RGB', (img.width * tile_size, img.height * tile_size))
     y_offset = 0
+    color_cache = {}
     for row in range(img.height):
         x_offset = 0
+
         row_im = Image.new("RGB", (img.width * tile_size, tile_size))
+
         for col in range(img.width):
             cc = pix[col, row]
             print(cc)
-            p_block = find_closest_color_in_json(color= cc, palette_items=d)
-            block_im = _load_block_texture(blocks_dir, p_block)
+            block_name = color_cache.get(cc)
+            if block_name == None:
+                block_name = find_closest_color_in_json(color= cc, palette_items=d)
+                color_cache[cc] = block_name
+
+            block_im = _load_block_texture(blocks_dir, block_name)
             row_im.paste(block_im, (x_offset, 0))
             x_offset += tile_size
         
